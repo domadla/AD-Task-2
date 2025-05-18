1d07dbfc9e7a3b4bea3ed0ec9bf0fc9e60eb1c48
domadla <domadlangbayan@gmail.com>
2025-05-18 13:42:17 +0800

    feat: add footer component

--- components/templates/foot.component.php
+++ components/templates/foot.component.php
@@ -1,10 +1,18 @@
 <?php
 function footer()
 {
-    ?>
-    </body>
+?>
+    <footer>
+        <div class="footer-container">
+            <p>&copy; 2025 Sixth String. All rights reserved.</p>
+            <p>Contact us: <a href="mailto:info@sixthstring.com">info@sixthstring.com</a> |
+                <a href="https://facebook.com/sixthstring" target="_blank">Facebook</a> |
+                <a href="https://instagram.com/sixthstring" target="_blank">Instagram</a>
+            </p>
+        </div>
+    </footer>
 
     </html>
-    <?php
+<?php
 }
 ?>
