59d966379757ca02154a247d7999b32e53100a80
domadla <domadlangbayan@gmail.com>
2025-05-18 14:55:37 +0800

    refactor: Added Landing Page greeting and decription

--- index.php
+++ index.php
@@ -12,7 +12,11 @@ head('SixthString');
 navHeader($navList);
 ?>
 
-
+<main class="homepage-main">
+    <h1>Welcome to SixthString!</h1>
+    <p>Your one-stop shop for quality <strong>Acoustic</strong> and <strong>Electric guitars</strong>.</p>
+    <p>Browse our collection and find the perfect guitar for your sound.</p>
+</main>
 
 <?php
 footer();
